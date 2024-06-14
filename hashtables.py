6ca1090da1c2c0118fc4bcab8cc0c9707e118e07
Mackintosh <simonib20@gmail.com>
2024-06-14 16:14:42 +0200

    simple implementation of hashtables with chaining

--- hashtables.py
+++ hashtables.py
@@ -1,8 +1,8 @@
 class HashtTable:
 
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0 
@@ -12,23 +12,28 @@ class HashtTable:
 
     def __setitem__(self, key, val):
         h = self.get_hash(key)
-        self.arr[h] = val
+        found = False
+        for idx , el in enumerate(self.arr[h]):
+            if len(el) == 2 and el[0] == key:
+                self.arr[h][idx] = (key, val)
+                found = True
+                break
+        if not found:
+            self.arr[h].append((key,val))
 
     def __getitem__(self, key):
         h = self.get_hash(key)
 
-        return self.arr[h]
+        for el in self.arr[h]:
+            if el[0] == key:
+                return el[1]
 
     def __delitem__(self, key):
         h = self.get_hash(key)
-        self.arr[h] = None
+        for index, el in enumerate(self.arr[h]):
+            if el[0] == key:
+                del self.arr[h][index]
     
-    def display(self):
-        for kv in list(enumerate(self.arr)):
-            if kv[1] is not None:
-                print(f"{kv[0]} : {kv[1]}")
-            
-
 
 
 
@@ -41,12 +46,16 @@ t['Elian'] = 195
 t['Jetnor'] = 167
 t['Fredi'] = 178
 t['Enri'] = 182
+t['March 6'] = 10
+t['March 17'] = 30
+
 
 print('hashtable after adding my friends height')
-t.display()
-
-del t['Elian']
+print(t.arr)
+del t['March 17']
 
 print("hashtable after removing one")
-t.display()
+print(t.arr)
+
+print('value at march 6: ',t['March 6'])
 
