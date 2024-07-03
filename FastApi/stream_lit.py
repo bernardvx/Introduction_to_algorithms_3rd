d28ee8e996f94ac5b6632b5881e925870c7b6791
Mackintosh <simonib20@gmail.com>
2024-07-03 21:07:38 +0200

    little streamlit ui for the project

--- FastApi/stream_lit.py
+++ FastApi/stream_lit.py
@@ -13,8 +13,36 @@ option = st.selectbox("What you want to do?",
 
 if "Create User" in option:
     st.write("you  want to create a new user: ", option)
+    name  = st.text_input("Name")
+    if name:
+        response = requests.post(f"{backend_url}/users/", json={"name": name})
+        if response.status_code == 200:
+            st.write("You have added a new user", response.json())
+        else:
+            st.error("Error adding a new user")
+            st.write(response.json())
 
-#st.write("you selected: ", option)
+
+if "Show my videos" in option:
+    user_id = st.text_input("Your user id")
+    if user_id:
+        response = requests.get(f"{backend_url}/users/{user_id}")
+        if response.status_code == 200:
+            st.write("These are your videos", response.json())
+        else:
+            st.error("Error showing your videos")
+            st.write(response.json())
+
+if "Upload Videos" in option:
+    owner_id = st.text_input("Your user id")
+    video_url = st.text_input("Your video url")
+    if video_url:
+        response = requests.post(f"{backend_url}/users/{owner_id}/videos", json={"user_id": owner_id, "url": video_url})
+        if response.status_code == 200:
+            st.write("video uploaded", response.json())
+        else:
+            st.error("Error uploading your video")
+            st.write(response.json())
 
 
 
