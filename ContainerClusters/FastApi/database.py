5a9554c6a9818fd9734a53611a8b9fd3eea4131b
Mackintosh <simonib20@gmail.com>
2024-07-24 15:33:53 +0200

    minikube cluster for backend

--- ContainerClusters/FastApi/database.py
+++ ContainerClusters/FastApi/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import time
+import os
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:mackintosh@db:5432/fastapi'
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 
 def create_conn():
@@ -16,9 +18,12 @@ def create_conn():
         except:
             print("could not connect to database\ntrying again...")
             time.sleep(5)
+    print("connected to database")
     return engine
 
 engine = create_conn()
+
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
